47daecf38125e7eaa29fee4aa06be3f6b14e4515
Mathieu Corre <mathieu.corre@epita.fr>
2019-04-09 09:54:40 +0200

    Initial code, no additionnal HW yet.

---
+++ hot_stamping.ino
@@ -0,0 +1,101 @@
+#define HYSTERESIS 10
+#define UPDATE_PERIOD 1000
+//#define CALIBRATION_FACTOR 1
+//#define CALIBRATION_OFFSET 0
+#define POT_RANGE_LOW 30
+#define POT_RANGE_HIGH 70
+#define RELAY_PIN 13
+#define THERMISTOR_PIN A0
+#define POTENTIOMETER_PIN A1
+#define NUMSAMPLES 5
+#define SERIESRESISTOR 10000
+#define THERMISTORNOMINAL 10000
+#define BCOEFFICIENT 3950
+#define TEMPERATURENOMINAL 25
+
+void setup()
+{
+	Serial.begin(9600);
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(THERMISTOR_PIN, INPUT);
+  pinMode(POTENTIOMETER_PIN, INPUT);
+ //pinmodes
+}
+
+uint8_t read_temp(uint8_t pin)
+{
+  uint32_t average = 0;
+  for (int i = 0; i < NUMSAMPLES; i++)
+  {
+    average += analogRead(pin);
+    delay(1);
+  }
+  average /= NUMSAMPLES;
+  average = 1023 / average - 1;
+  average = SERIESRESISTOR / average;
+  return average;
+}
+
+uint8_t read_measured()
+{
+  uint8_t measured = read_temp(THERMISTOR_PIN);
+  Serial.print("Thermistor resistance: "); 
+  Serial.print(measured);
+  Serial.println("Ohms");
+  float steinhart;
+  steinhart = measured / THERMISTORNOMINAL;     // (R/Ro)
+  steinhart = log(steinhart);                  // ln(R/Ro)
+  steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+  steinhart = 1.0 / steinhart;                 // Invert
+  steinhart -= 273.15;
+  //measured = steinhart;
+	//return measured;
+  return steinhart;
+}
+
+uint8_t read_target()
+{
+  uint8_t target = read_temp(POTENTIOMETER_PIN);
+  target = map(target, 0, 1023, POT_RANGE_LOW, POT_RANGE_HIGH);
+	return target;
+}
+
+void control_relay(uint8_t measured, uint8_t target)
+{
+	static bool activated = true;
+	if (activated)
+	{
+		if (measured + HYSTERESIS > target)
+			activated = false;
+	}
+	else
+	{
+		if (measured - HYSTERESIS < target)
+			activated = true;
+		
+	}
+	Serial.print("Relay is in the ");
+	Serial.print(activated ? "on" : "off");
+	Serial.println(" state.");
+	//write !activated on pin (relay must be off by default).
+  digitalWrite(RELAY_PIN, !activated);
+}
+
+void print_temperatures(uint8_t measured, uint8_t target)
+{
+	Serial.print("Measured temp: ");
+	Serial.print(measured);
+	Serial.print(" *C; Target temp: ");
+	Serial.print(target);
+	Serial.println(" *C");
+}
+
+void loop()
+{
+	uint8_t measured = read_measured();
+	uint8_t target = read_target();
+	print_temperatures(measured, target);
+	control_relay(measured, target);
+	delay(UPDATE_PERIOD); //Put to sleep?
+}
